368565af2064ffc908c46667c4b316a795b30c3a
Bikram Pun <bikrampun47@gmail.com>
2023-04-19 21:24:26 +0545

    LSP example

---
+++ DesignPrinciple/LiskovSubstitutionPrincipleExample/src/com/example/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example;
+
+abstract class Vehicle {
+    abstract int getSpeed();
+    abstract int getCubicCapacity();
+
+    public static void main (String [] args) {
+
+        Vehicle vehicle = new Bus();
+        vehicle.getSpeed();
+        vehicle = new Car();
+        vehicle.getCubicCapacity();
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    int getSpeed() { return 0; }
+
+    @Override
+    int getCubicCapacity() { return 0; }
+    boolean sHatchBack() { return true; }
+}
+
+class Bus extends Vehicle {
+    int getSpeed() { return 0; }
+    int getCubicCapacity() { return 0; }
+    String getEmergencyExitLoc() { return  "";}
+}
