923279bb0a7eac9edda77bb14980de90e38d0a71
Bikram Pun <bikrampun47@gmail.com>
2023-04-19 21:12:24 +0545

    Single Responsibility Example

---
+++ DesignPrinciple/SingleResponsibilityExample/src/com/example/Finance.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Finance {
+
+    public double calcIncomeTaxForCurrentYear(Employee emp) {
+        // income tax logic implementation is using Employee info passed in
+    }
+}
---
+++ DesignPrinciple/SingleResponsibilityExample/src/com/example/HRPromotions.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HRPromotions {
+
+    public boolean isPromotionDueThisYear(Employee emp) {
+        // promotion logic implementation is using Employee info passed in
+    }
+}
